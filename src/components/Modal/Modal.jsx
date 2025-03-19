bad8bea81d88edefd184b44cddc1f2e7962063a6
Seongbin Kim <kim968968@gmail.com>
2025-03-19 19:47:12 +0900

    list 모달 버튼 수정

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -8,34 +8,38 @@ const Modal = () => {
 
   if (!isModalOpen) return null; // 모달이 열려있지 않으면 렌더링하지 않음
 
+  const handleSort = (order) => {
+    setOrderBy(order);
+    toggleModal();
+  };
+
   return (
     <>
-      <div className="modal__overlay" onClick={toggleModal}>
+      <div className="modal__overlay">
         <div className="modal__box">
           <div className="modal__detail">
             <p className="modal__sorting">정렬</p>
-            <button
-              className={`sort__button ${orderBy === 'recent' ? 'selected' : ''}`}
-              onClick={() => setOrderBy('recent')}
-            >
-              최신순
-            </button>
-            <hr />
-            <button
-              className={`sort__button ${orderBy === 'likes' ? 'selected' : ''}`}
-              onClick={() => setOrderBy('likes')}
-            >
-              좋아요순
-            </button>
-            <hr />
-            <button
-              className={`sort__button ${orderBy === 'productsCount' ? 'selected' : ''}`}
-              onClick={() => setOrderBy('productsCount')}
-            >
-              등록된 상품순
-            </button>
-            <hr />
-            <button className="modal__closeButton">
+            <ul>
+              <li
+                className={`sort__button ${orderBy === 'recent' ? 'selected' : ''}`}
+                onClick={() => handleSort('recent')}
+              >
+                최신순
+              </li>
+              <li
+                className={`sort__button ${orderBy === 'likes' ? 'selected' : ''}`}
+                onClick={() => handleSort('likes')}
+              >
+                좋아요순
+              </li>
+              <li
+                className={`sort__button ${orderBy === 'productsCount' ? 'selected' : ''}`}
+                onClick={() => handleSort('productsCount')}
+              >
+                등록된 상품순
+              </li>
+            </ul>
+            <button className="modal__closeButton" onClick={toggleModal}>
               <img src={close} alt="close"></img>
             </button>
           </div>
